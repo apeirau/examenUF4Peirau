5ead7ca48c474b9458decba3f19817976b684502
Alex Peirau <alexpeirau00@gmai.com>
2022-05-16 08:44:01 +0200

    Añadido class Cuadrado en branca00

--- shape.java
+++ shape.java
@@ -1,28 +1,63 @@
 // A sample class Shape which provides a method to get the Shape's area
 class Shape {
+
+  /**
+   * @author alexpeirau
+   *         Código de el examen M08_UF4
+   * @return double
+   */
   public double getArea() {
     return 0;
   }
 }
+
 // A Rectangle is a Shape with a specific width and height
-class Rectangle extends Shape {   // extended form the Shape class
+class Rectangle extends Shape { // extended form the Shape class
   private double width;
   private double height;
+
   public Rectangle(double width, double height) {
     this.width = width;
     this.height = height;
   }
+
   public double getArea() {
-    return width * height; 
+    return width * height;
   }
 }
+
 // A Circle is a Shape with a specific radius
 class Circle extends Shape {
   private double radius;
+
   public Circle(double radius) {
-    this.radius = radius; 
+    this.radius = radius;
   }
+
   public double getArea() {
-    return 3.14 * radius * radius; 
+    return 3.14 * radius * radius;
+  }
+}
+
+// Class del Cuadrado
+
+class Cuadrado extends Shape {
+  private double lado;
+  private double areaCuadrado;
+
+  public Cuadrado(double lado) {
+    this.lado = lado;
+  }
+
+  public double getLado() {
+    return lado;
+  }
+
+  public void setLado(double lado) {
+    this.lado = lado;
+  }
+
+  public double getareaCuadrado() {
+    return areaCuadrado;
   }
 }
